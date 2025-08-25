44b2757e31dd2b57b168306d06d059d93e5b24a4
koguchi-e <koguchi.e915@gmail.com>
2025-08-25 11:49:07 +0900

    定数を廃止してクラスを作成、XSS対策が動作するよう修正

--- app.rb
+++ app.rb
@@ -7,97 +7,112 @@ require 'json'
 use Rack::MethodOverride
 helpers ERB::Util
 
-MEMOS = []
+class MemoApp < Sinatra::Base
+  helpers ERB::Util
 
-get '/' do
-  @memos = MEMOS
-  erb :index
-end
+  @memos = []
 
-get '/new' do
-  @memo = MEMOS
-  erb :new
-end
-
-class Memo
-  attr_accessor :id, :title, :body
-
-  def initialize(id, title, body)
-    @id = id
-    @title = title
-    @body = body
+  class << self
+    attr_accessor :memos
   end
-end
 
-post '/new' do
-  title = params[:title].to_s.strip
-  body = params[:body].to_s.strip
-
-  unless title.empty?
-    new_memo = Memo.new(MEMOS.size + 1, title, body)
-    MEMOS << new_memo
-    save_memos_to_json
+  get '/' do
+    @memos = self.class.memos
+    erb :index
   end
-  redirect '/'
-end
 
-get '/show/:id' do
-  @memo = MEMOS.find { |m| m.id == params[:id].to_i }
-  if @memo
-    erb :show
-  else
-    erb :not_found
+  get '/new' do
+    @memos = self.class.memos
+    erb :new
   end
-end
 
-get '/edit/:id' do
-  @memo = MEMOS.find { |m| m.id == params[:id].to_i }
-  if @memo
-    erb :edit
-  else
-    erb :not_found
+  class Memo
+    attr_accessor :id, :title, :body
+
+    def initialize(id, title, body)
+      @id = id
+      @title = title
+      @body = body
+    end
   end
-end
 
-delete '/delete/:id' do
-  @memo = MEMOS.find { |m| m.id == params[:id].to_i }
-  if @memo
-    MEMOS.delete(@memo)
+  post '/new' do
+    @memos = self.class.memos
+    title = params[:title].to_s.strip
+    body = params[:body].to_s.strip
+
+    unless title.empty?
+      new_memo = Memo.new(@memos.size + 1, title, body)
+      @memos << new_memo
+      save_memos_to_json
+    end
     redirect '/'
-  else
+  end
+
+  get '/show/:id' do
+    @memos = self.class.memos
+    @memo = @memos.find { |m| m.id == params[:id].to_i }
+    if @memo
+      erb :show
+    else
+      erb :not_found
+    end
+  end
+
+  get '/edit/:id' do
+    @memos = self.class.memos
+    @memo = @memos.find { |m| m.id == params[:id].to_i }
+    if @memo
+      erb :edit
+    else
+      erb :not_found
+    end
+  end
+
+  delete '/delete/:id' do
+    @memos = self.class.memos
+    @memo = @memos.find { |m| m.id == params[:id].to_i }
+    if @memo
+      @memos.delete(@memo)
+      redirect '/'
+    else
+      erb :not_found
+    end
+  end
+
+  post '/update/:id' do
+    @memos = self.class.memos
+    @memo = @memos.find { |m| m.id == params[:id].to_i }
+    if @memo
+      @memo.title = params[:title]
+      @memo.body = params[:body]
+      redirect "/show/#{@memo.id}"
+    else
+      erb :not_found
+    end
+  end
+
+  def save_memos_to_json
+    @memos = self.class.memos
+    memos = @memos.map do |memo|
+      {
+        id: memo.id,
+        title: memo.title,
+        body: memo.body
+      }
+    end
+    json_data = JSON.pretty_generate(memos)
+    File.open('memos.json', 'w') do |file|
+      file.write(json_data)
+    end
+  end
+
+  get '/download_memos' do
+    send_file 'memos.json', type: 'application/json', disposition: 'attachment'
+  end
+
+  not_found do
     erb :not_found
   end
 end
-
-post '/update/:id' do
-  @memo = MEMOS.find { |m| m.id == params[:id].to_i }
-  if @memo
-    @memo.title = params[:title]
-    @memo.body = params[:body]
-    redirect "/show/#{@memo.id}"
-  else
-    erb :not_found
-  end
-end
-
-def save_memos_to_json
-  memos = MEMOS.map do |memo|
-    {
-      id: memo.id,
-      title: memo.title,
-      body: memo.body
-    }
-  end
-  json_data = JSON.pretty_generate(memos)
-  File.open('memos.json', 'w') do |file|
-    file.write(json_data)
-  end
-end
-
-get '/download_memos' do
-  send_file 'memos.json', type: 'application/json', disposition: 'attachment'
-end
-
-not_found do
-  erb :not_found
-end
+MemoApp.run! if __FILE__ == $PROGRAM_NAME
